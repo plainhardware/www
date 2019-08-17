29b8fcd5080f258409563250cf345ab5d4fa0880
Jonathan Abrahams <jonathan@plain-hardware.co.za>
2019-08-17 20:30:10 +0200

    favicon

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const HWP = require('html-webpack-plugin');
 module.exports = {
     plugins: [
         new HWP({
-            title: 'Plain Hardware | Hobby and Electronic store'
+            favicon: path.resolve(__dirname, 'src', 'favicon.ico'),
+            title: 'Plain Hardware | Hobby and Electronic store',
+            template: path.resolve(__dirname, 'src', 'index.html')
         })
     ]
 }

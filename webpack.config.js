c44efc06ee9bcf2c99b4564798ce8d89b68a9cee
Jonathan Abrahams <jonathan@plain-hardware.co.za>
2019-08-17 21:14:08 +0200

    assets

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,27 @@ const path = require('path');
 const HWP = require('html-webpack-plugin');
 
 module.exports = {
+    module: {
+        rules: [{
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[contenthash].[ext]',
+                    outputPath: 'static/assets/',
+                    publicPath: 'static/assets/',
+                    postTransformPublicPath: (p) => `__webpack_public_path__ + ${p}`,
+                  }
+            }]
+        }]
+    },
     plugins: [
         new HWP({
+            templateParameters: {
+                title: 'Plain Hardware | Hobby and Electronic store',
+                logo: path.resolve(__dirname, 'src', 'assets', 'logo.png'),
+            },
             favicon: path.resolve(__dirname, 'src', 'favicon.ico'),
-            title: 'Plain Hardware | Hobby and Electronic store',
             template: path.resolve(__dirname, 'src', 'index.html')
         })
     ]

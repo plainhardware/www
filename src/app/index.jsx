f60d2dd7dc9cb179daaf7d3b81e368d2fe448982
Jonathan Abrahams <jonathan@plain-hardware.co.za>
2019-08-20 20:42:55 +0200

    cachegroup

--- src/app/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import './index.scss'
-
-const App = () => <div className="app">Welcome</div>
-
-export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import "normalize-scss/sass/normalize/_import-now.scss";
 import './index.scss'
-import App from './app';
+import App from './app/App';
 
 render(<App />, document.getElementById('app'))

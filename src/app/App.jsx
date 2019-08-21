5719e0792bdc6796b7974ff75a6d2d561266ec3a
Jonathan Abrahams <jonathan@plain-hardware.co.za>
2019-08-21 14:48:00 +0200

    minimal material-ui

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,36 +1,12 @@
 import React from 'react'
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
 
 import './app.scss'
 
-function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-                Your Website
-        </Link>{' '}
-            {new Date().getFullYear()}
-            {'. Built with '}
-            <Link color="inherit" href="https://material-ui.com/">
-                Material-UI.
-        </Link>
-        </Typography>
-    );
-}
-
-const App = () => {
-    <Container maxWidth="sm">
-        <Box my={4}>
-            <Typography variant="h4" component="h1" gutterBottom>
-                Create React App v4-beta example
-        </Typography>
-            <Copyright />
-        </Box>
-    </Container>
-}
+const App = () => (
+  <Button variant="contained" color="primary">
+    Hello World
+  </Button>
+);
 
 export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/styles';
-
 import './index.scss'
 import App from './app/App';
-import theme from './theme';
 
-render(
-    <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-    </ThemeProvider>, document.querySelector('#app'))
+render(<App />, document.querySelector('#app'))
